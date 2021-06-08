fcabb76ac5d38c06c2b813e48b3c86964cc4a537
s119605 <s119605@student.hsleiden.nl>
2021-06-08 15:15:17 +0200

    code is er, alleen nog toegang krijgen tot api

--- app/src/main/java/com/example/chauffeursapp/MainActivity.java
+++ app/src/main/java/com/example/chauffeursapp/MainActivity.java
@@ -3,12 +3,77 @@ package com.example.chauffeursapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email,pass;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = (EditText) findViewById(R.id.txtEmail);
+        pass = (EditText) findViewById(R.id.txtPassword);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                login(email.getText().toString(), pass.getText().toString());
+            }
+        });
+        
     }
+
+    public void login(String email, String pass){
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        String url = "http://127.0.0.1:8000/api/login";
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST,url,
+            new Response.Listener<String>(){
+                @Override
+                public void onResponse(String response){
+                    Log.d("APPLOG", response.toString());
+                    Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                }
+            },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        Log.d("APPLOG", error.toString());
+                        Toast.makeText(MainActivity.this, "Fail to get response = " + error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String,String>();
+                params.put("post_email", email);
+                params.put("post_passwd", pass);
+                return params;
+            }
+        };
+        queue.add(postRequest);
+    }
+
 }
