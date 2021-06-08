9d96b6fe7715e02b47803a22eb95b49db32363ea
prgnl <alexfilinsky@gmail.com>
2021-06-08 12:36:46 +0200

    eerste push met werkende knop op mainactivity
    als test dat alles goed werkt, maak ik deze push. in volgende versie wordt de timer geimplementeerd

---
+++ app/src/main/java/com/example/chauffeursapp/ActivityTimer.java
@@ -0,0 +1,13 @@
+package com.example.chauffeursapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityTimer extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstances) {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.activity_timer);
+    }
+}
--- app/src/main/java/com/example/chauffeursapp/MainActivity.java
+++ app/src/main/java/com/example/chauffeursapp/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.chauffeursapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button toTimerScreenButton = findViewById(R.id.toTimerScreen);
+        toTimerScreenButton.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        Bundle bundleForTimerScreen = new Bundle();
+        String name = "Alex";
+        Log.d("to timer", "went to timer");
+        bundleForTimerScreen.putString("name", name);
+        Intent toTimerScreenIntent = new Intent(this, ActivityTimer.class);
+        startActivity(toTimerScreenIntent);
     }
 }
