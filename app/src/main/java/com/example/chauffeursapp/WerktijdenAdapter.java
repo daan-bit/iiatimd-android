af78f1208f516890427b0dbaa44bf2f562488fdd
prgnl <alexfilinsky@gmail.com>
2021-07-02 11:53:42 +0200

    bug gefixt
    -het werkt
    -er moet nog aan gewerkt worden dat het per user is en niet voor alle users

--- app/src/main/java/com/example/chauffeursapp/WerktijdenAdapter.java
+++ app/src/main/java/com/example/chauffeursapp/WerktijdenAdapter.java
@@ -19,24 +19,27 @@ public class WerktijdenAdapter extends RecyclerView.Adapter<WerktijdenAdapter.We
 
     public static class WerktijdenViewHolder extends RecyclerView.ViewHolder {
         public TextView textView;
+        public TextView textView2;
 
         public WerktijdenViewHolder(View v) {
             super(v);
             textView = v.findViewById(R.id.textViewWerknemer);
+            textView2 = v.findViewById(R.id.textViewWerknemer2);
         }
     }
 
     @NonNull
     @Override
     public WerktijdenViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_geschiedenisadminusercard, parent, false);
+        View v = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_geschiedenisusercard, parent, false);
         WerktijdenViewHolder werktijdenViewHolder = new WerktijdenViewHolder(v);
         return werktijdenViewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull WerktijdenViewHolder holder, int position) {
-        holder.textView.setText(werktijden[position].getId());
+        holder.textView.setText("Van: " + werktijden[position].getBegin_shift());
+        holder.textView2.setText("Tot: " + werktijden[position].getEinde_shift());
     }
 
     @Override
